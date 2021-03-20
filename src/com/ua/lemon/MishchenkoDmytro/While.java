1b71e0f45a09788f5765c1829aac6748004ec28a
Dima Mishchenko <dima.mishchenko32@gmail.com>
2021-03-20 11:38:01 +0200

    While

---
+++ src/com/ua/lemon/MishchenkoDmytro/While.java
@@ -0,0 +1,14 @@
+package com.ua.lemon.MishchenkoDmytro;
+
+public class While {
+    public static void main(String[] args) {
+        int a = 0;
+        while(a<10)
+        {
+            System.out.println(a);
+            a++;
+
+        }
+
+    }
+}
