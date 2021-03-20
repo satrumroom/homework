bd63a41edb04435fc75167cbbb27351b58e180e1
Yana Ponomarenko <marvelous1creature@gmail.com>
2021-03-20 11:10:30 +0200

    test

---
+++ src/com/ua/lemon/yana/Homework_1_1.java
@@ -0,0 +1,21 @@
+package com.ua.lemon.yana;
+import java.time.LocalDate;
+import java.util.Scanner;
+public class Homework_1_1 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = in.nextLine();
+        System.out.print("Enter your surname: ");
+        String surname = in.nextLine();
+        System.out.print("Enter your patronymic: ");
+        String patronymic = in.nextLine();
+        System.out.print("Enter your age: ");
+        int age = in.nextInt();
+        LocalDate date = LocalDate.now();
+        int year = date.getYear();
+        int yearOfBirth = year - age;
+        System.out.println("Hello, dear " + surname + " " + name + " " + patronymic + "!");
+        System.out.println("Year of birth is - " + yearOfBirth);
+    }
+}
