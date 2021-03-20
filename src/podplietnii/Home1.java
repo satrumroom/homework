f140c453d6f59d3d0efade42bfd4f9cb38987d5d
Yevheniy_Podplietnii <magisckyevgeshka@gmail.com>
2021-03-20 10:46:42 +0200

    Homework 1

---
+++ src/podplietnii/Home1.java
@@ -0,0 +1,31 @@
+package podplietnii;
+
+import java.util.Scanner;
+
+public class Home1 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String firstName ;
+        String surname ;
+        String patronymic ;
+        int age;
+        int birthYear;
+
+        System.out.print("Enter your first name: ");
+        firstName = scan.next();
+        System.out.print("Enter your surname: ");
+        surname = scan.next();
+        System.out.print("Enter your patronymic: ");
+        patronymic = scan.next();
+        System.out.print("Enter your age: ");
+        age = scan.nextInt();
+
+        birthYear = 2021 - age;
+
+        System.out.printf("Hello, %s %s %s ,who was born in %d year.", firstName, surname, patronymic, birthYear);
+
+    }
+}
