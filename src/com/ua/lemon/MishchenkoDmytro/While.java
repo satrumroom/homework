8bf7a5591c26bb528a93be1f383f20f9c7d2fbdb
Dima Mishchenko <dima.mishchenko32@gmail.com>
2021-03-20 12:46:00 +0200

    Array

--- src/com/ua/lemon/MishchenkoDmytro/While.java
+++ src/com/ua/lemon/MishchenkoDmytro/While.java
@@ -2,7 +2,18 @@ package com.ua.lemon.MishchenkoDmytro;
 
 public class While {
     public static void main(String[] args) {
-        int a = 0;
+        final int a = 57;
+        for(int i = a; i<1327; i++)
+        {
+            if(i%5 == 0)
+                continue;
+            if(i%33 == 0)
+                break;
+            System.out.println(i);
+
+
+        }
+
 
     }
 }
