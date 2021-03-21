c4d1d69a86a1b9843d1f7431a5b39cb0dc7cbc3e
Yevheniy_Podplietnii <magisckyevgeshka@gmail.com>
2021-03-21 22:02:41 +0200

    Homework 3.2

---
+++ src/com/ua/lemon/podplietnii/third/homework/Practice3_2.java
@@ -0,0 +1,9 @@
+package com.ua.lemon.podplietnii.third.homework;
+
+public class Practice3_2 {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            System.out.print(i + " ");
+        }
+    }
+}
