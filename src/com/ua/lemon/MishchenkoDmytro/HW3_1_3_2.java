1b17f4f1e0e632af5afb9edf1b87d4144e4588b6
Dima Mishchenko <dima.mishchenko32@gmail.com>
2021-03-23 17:24:00 +0200

    HW3_1,2

---
+++ src/com/ua/lemon/MishchenkoDmytro/HW3_1_3_2.java
@@ -0,0 +1,18 @@
+package com.ua.lemon.MishchenkoDmytro;
+
+public class HW3_1_3_2 {
+    public static void main(String[] args) {
+
+         for(int i = 1;i<=5;i++)
+             System.out.print(i+" ");
+
+        System.out.println();
+         for(int i = 5;i>=1;i--)
+            System.out.print(i+" ");
+
+
+
+
+    }
+
+}
